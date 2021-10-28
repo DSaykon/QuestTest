b60ae4504157dc8269f0736887139535c782b260
DSaykon <saykon@mail.ru>
2021-10-28 17:17:17 +1000

    1.13

--- interface.py
+++ interface.py
@@ -34,8 +34,14 @@ class TestInterface:
             self.set_check_btn()
             self.lbl_checked.config(text="\nНабрано баллов: {} / {}".format(self.score, self.n + 1))
         else:
-            print("Вопросов больше нет")
             self.end()
+
+    # обновления для каждого нового вопроса в тесте
+    def next_quest(self):
+        self.n += 1
+        self.variant.set(0)
+        self.remove_check_btn()
+        self.change_lbl_text()
 
     # создание выбора через галочку
     def set_check_btn(self):
@@ -44,22 +50,6 @@ class TestInterface:
                              command=self.checked)
             ch.pack()
             self.checkbtn_list.append(ch)
-
-    # очистка окна от галочек
-    def remove_check_btn(self):
-        if self.checkbtn_list:
-            for ch in self.checkbtn_list:
-                ch.destroy()
-            self.checkbtn_list.clear()
-
-    # изменение цвета при проверке ответа
-    def change_check_btn(self):
-        for ch in self.checkbtn_list:
-            ch.config(state=DISABLED)
-            if ch["onvalue"] == test.test_question_list[self.n].correct_variant:
-                ch.config(disabledforeground="#000", bg="#0f0")
-            elif ch["onvalue"] == self.variant.get():
-                ch.config(disabledforeground="#000", bg="#f00")
 
     # проверка правильности ответа
     def checked(self):
@@ -72,6 +62,30 @@ class TestInterface:
             self.btn.config(state=NORMAL)
         self.change_check_btn()
 
+    # изменение цвета при проверке ответа
+    def change_check_btn(self):
+        for ch in self.checkbtn_list:
+            ch.config(state=DISABLED)
+            if ch["onvalue"] == test.test_question_list[self.n].correct_variant:
+                ch.config(disabledforeground="#000", bg="#0f0")
+            elif ch["onvalue"] == self.variant.get():
+                ch.config(disabledforeground="#000", bg="#f00")
+
+    # очистка окна от галочек
+    def remove_check_btn(self):
+        if self.checkbtn_list:
+            for ch in self.checkbtn_list:
+                ch.destroy()
+            self.checkbtn_list.clear()
+
+    # Завершение теста и вывод результата
+    def end(self):
+        self.remove_check_btn()
+        self.lbl_checked.config(text="Тест завершён.\nНабрано баллов: {} / {}".format(self.score, self.n))
+        self.lbl_text.set("Вопросы закончились.\nДля прохождения другого случайного теста - перезапустите программу."
+                          "\nДля прохождения теста повторно - нажмите кнопку внизу.")
+        self.btn.config(text="Пройти этот тест снова", command=self.reset)
+
     # обнуляет переменные в начальное положение
     def reset(self):
         self.n = 0
@@ -81,18 +95,3 @@ class TestInterface:
         self.lbl_text.set('{}\nДля начала нажмите "Старт"'.format(test.title))
         self.lbl_checked.config(text='')
         self.btn.config(text="Старт", command=self.change_lbl_text)
-
-    # обновления для каждого нового вопроса в тесте
-    def next_quest(self):
-        self.n += 1
-        self.variant.set(0)
-        self.remove_check_btn()
-        self.change_lbl_text()
-
-    # Завершение теста и вывод результата
-    def end(self):
-        self.remove_check_btn()
-        self.lbl_checked.config(text="Тест завершён.\nНабрано баллов: {} / {}".format(self.score, self.n))
-        self.lbl_text.set("Вопросы закончились.\nДля прохождения другого случайного теста - перезапустите программу."
-                          "\nДля прохождения теста повторно - нажмите кнопку внизу.")
-        self.btn.config(text="Пройти этот тест снова", command=self.reset)
