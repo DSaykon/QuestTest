5dde9f7f8d851b80f1e2e203f7833ece602e3ef8
DSaykon <saykon@mail.ru>
2021-10-28 13:54:39 +1000

    1.1

--- core.py
+++ core.py
@@ -1,25 +1,35 @@
 from random import randint
 from testlibrary import *
 
+class Test:
+    def __init__(self, title='test', test_question_list=None):
+        if test_question_list is None:
+            test_question_list = []
+        self.title = title
+        self.test_question_list = test_question_list
+        self.total_questions = len(test_question_list)
+
 
 a = randint(1, 10)
 if a == 1:
-	test_question_list = test_1
+    test_question_list = test_1
 elif a == 2:
-	test_question_list = test_2
+    test_question_list = test_2
 elif a == 3:
-	test_question_list = test_3
+    test_question_list = test_3
 elif a == 4:
-	test_question_list = test_4
+    test_question_list = test_4
 elif a == 5:
-	test_question_list = test_5
+    test_question_list = test_5
 elif a == 6:
-	test_question_list = test_6
+    test_question_list = test_6
 elif a == 7:
-	test_question_list = test_7
+    test_question_list = test_7
 elif a == 8:
-	test_question_list = test_8
+    test_question_list = test_8
 elif a == 9:
-	test_question_list = test_9
+    test_question_list = test_9
 else:
-	test_question_list = test_10
+    test_question_list = test_10
+
+test = Test("Программа тестирования", test_question_list)
